7b5a5a59cc34facc86436d5076b6ed201081f4d0
ashgodkin <ashgodkin@hotmail.com>
2023-12-13 16:15:10 +0000

    Mongo Lab Exercises Done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -2,9 +2,12 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
+const pwValidator = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true, match: [pwValidator] }
 });
 
 export default mongoose.model('User', UserSchema);
--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import usersRouter from './api/users';
 import './db';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.json());
 
