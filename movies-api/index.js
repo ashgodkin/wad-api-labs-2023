7a32a2c66cceb9049db58ad6186229b2f8d46d35
ashgodkin <ashgodkin@hotmail.com>
2023-12-14 20:30:33 +0000

    Added movies router

--- movies-api/index.js
+++ movies-api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import usersRouter from './api/users';
-//import moviesRouter from './api/movies';
+import moviesRouter from './api/movies';
 import './db';
 import defaultErrHandler from './errHandler'
 
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
-//app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 
 app.use(defaultErrHandler);
 
